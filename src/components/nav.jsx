090a0bd24784a5381122a35c7dacb064d529e345
林岑影 <lincenying@qq.com>
2020-11-24 11:39:14 +0800

    on 2020-11-24 11:39:14

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import { Affix } from 'antd'
+
 const Nav = () => {
     return (
-        <h1>
-            <NavLink to="/" exact activeClassName="current">
-                列表
-            </NavLink>
-        </h1>
+        <Affix offsetTop={20}>
+            <h1>
+                <NavLink to="/" exact activeClassName="current">
+                    列表
+                </NavLink>
+            </h1>
+        </Affix>
     )
 }
 
